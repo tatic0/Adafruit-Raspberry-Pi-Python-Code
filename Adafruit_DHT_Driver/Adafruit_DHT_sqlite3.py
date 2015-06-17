0acb04ab16f810a75e2f31a0d056f08592f7a582
F Varas <fvaras@anevia.com>
2015-06-17 15:02:23 +0200

    working sqlite3 version

--- Adafruit_DHT_Driver/Adafruit_DHT_sqlite3.py
+++ Adafruit_DHT_Driver/Adafruit_DHT_sqlite3.py
@@ -15,7 +15,8 @@ try:
     #logfile=open('temp-hum.log','a')
     conn = sqlite3.connect("thg.db")
     # Run the DHT program to get the humidity and temperature readings!
-    output = subprocess.check_output(["./Adafruit_DHT", "22", "4"]);
+    #output = subprocess.check_output(["./Adafruit_DHT", "22", "4"]);
+    output = subprocess.check_output(["./Adafruit_DHT", "22", "4"]); ### DEBUG
     print output
     # search for temperature printout
     matches = re.search("Temp =\s+([0-9.]+)", output)
@@ -37,7 +38,8 @@ try:
     curs.execute("INSERT INTO sensordata ('date','temp','humidity') VALUES (?,?,?)", (now, temp, humidity))
     conn.commit()
     # Wait half an hour between each measurement
-    time.sleep(1800)
+    time.sleep(18) # DEBUG
+    #time.sleep(1800)
 except KeyboardInterrupt:
   print("You pressed ctrl-c, quitting")
   conn.close()
